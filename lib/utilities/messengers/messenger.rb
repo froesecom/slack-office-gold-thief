00cd46d6daa298c77507173c174063e8a65b50c8
Erik the Viking <erik@hhd.com.au>
2018-08-16 15:34:28 +1000

    avm fix bug in messenger utility

--- lib/utilities/messengers/messenger.rb
+++ lib/utilities/messengers/messenger.rb
@@ -1,7 +1,7 @@
 class OfficeGoldThief::Utilities::Messenger
 
   def self.submit_message(driver, message)
-    driver.execute_script("document.querySelectorAll('#msg_input p')[0].innerHTML = '#{task}'")
+    driver.execute_script("document.querySelectorAll('#msg_input p')[0].innerHTML = '#{message}'")
     driver.action.send_keys(:enter).perform
   end
 end
