92be09357dcf58694bd27bd4af87245b868f2a12
Erik the Viking <erik@hhd.com.au>
2018-08-17 17:41:28 +1000

    remove magic number

--- lib/utilities/info/user_info.rb
+++ lib/utilities/info/user_info.rb
@@ -7,7 +7,7 @@ class OfficeGoldThief::Utilities::UserInfo
   end
 
   def get_status(slack_user=nil)
-    remove_existing_status_elements
+    deidentify_existing_status_elements
     request_status(slack_user)
     parse_status(slack_user)
   end
@@ -20,14 +20,9 @@ class OfficeGoldThief::Utilities::UserInfo
   def parse_status(slack_user, tries=0)
     # refactor
     unless tries > 4
-      wait = Selenium::WebDriver::Wait.new(timeout: 10) # seconds
-      wait.until {
-        @driver.find_elements(class: @message_text_field_class)
-      }
       statuses = @driver.find_elements(class: @message_text_field_class)
       status_el = statuses.last
       status = status_el.text if status_el
-      puts tries
       if status && is_valid_status(status)
         parse_status_text(status)
       else
@@ -57,13 +52,12 @@ class OfficeGoldThief::Utilities::UserInfo
     OfficeGoldThief::Utilities::Messenger.submit_message(@driver, message)
   end
 
-  def remove_existing_status_elements
+  def deidentify_existing_status_elements
     info_fields = @driver.find_elements(class: @message_text_field_class)
     script = "arguments[0].setAttribute('class', '')"
     info_fields.each do |f|
       @driver.execute_script(script, f)
     end
-    sleep 2 # magic number to ensure everything is removed; refactor
   end
 
 end
